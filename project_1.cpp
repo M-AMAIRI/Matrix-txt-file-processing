2bb6ed7650bfb007afd1999c39c2eea65033c33e
M-AMAIRI <mr.amairi.ai@gmail.com>
2018-01-30 11:24:04 +0100

    initial project version

---
+++ project_1.cpp
@@ -0,0 +1,16 @@
+/* 
+M-AMAIRI 
+project_1.cpp : 
+.Dat file manipulation .
+The objects defined below may be used to read and write images to and from two - dimensional DAT files
+
+*/
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
